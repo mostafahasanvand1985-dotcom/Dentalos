6dc996e36df670a27312d7fec6cb6f3b7fc4e3e8
mostafahasanvand1985-dotcom <mostafahasanvand1985@gmail.com>
2025-08-29 23:30:50 +0330

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = 'dentalos-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './db.js',
+  './rbac.js',
+  './update-center.js',
+  './manifest.json',
+  './assets/icon-192.png',
+  './assets/icon-512.png',
+  './assets/logo.svg'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  )));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request))
+  );
+});
