62b65a9ee8c3b27c1062e45fea85b5ec18260195
mostafahasanvand1985-dotcom <mostafahasanvand1985@gmail.com>
2025-08-30 01:13:45 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,21 @@
-self.skipWaiting && self.skipWaiting();
-self.clients && self.clients.claim && self.clients.claim();
-
-const CACHE = 'dentalos-v60-1';
+const CACHE = 'dentalos-v1';
 const ASSETS = [
-  './','./index.html','./styles.css','./app.js','./db.js','./rbac.js','./update-center.js',
-  './manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./header-logo.png'
+  './','./index.html','./styles.css',
+  './app.js','./db.js','./rbac.js','./update-center.js',
+  './manifest.json'
 ];
 
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 self.addEventListener('activate', e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
 });
 self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+  const url=new URL(e.request.url);
+  if(url.origin===location.origin){
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
 });
