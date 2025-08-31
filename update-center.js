28ad2cde1db24c2463a76354c80706e938b4b8a3
mostafahasanvand1985-dotcom <mostafahasanvand1985@gmail.com>
2025-09-01 00:53:09 +0330

    Delete update-center.js

--- update-center.js
+++
@@ -1,8 +0,0 @@
-// مرکز بروزرسانی سبک (کانال‌ها + نسخه)
-const UPDATE={
-  channel: localStorage.getItem('upd.channel') || 'stable', // stable | beta
-  version: 'v60',
-  async check(){ return {latest:'v60', notes:'Initial GH Pages build'}; },
-  switch(ch){ this.channel=ch; localStorage.setItem('upd.channel',ch); }
-};
-window.UPDATE=UPDATE;
